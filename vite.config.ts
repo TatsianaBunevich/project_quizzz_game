452524808e78cea196652603d734d867f16e21a5
TatsianaBunevich <101735052+TatsianaBunevich@users.noreply.github.com>
2024-06-22 23:11:12 +0200

    added vitest environment

--- vite.config.ts
+++ vite.config.ts
@@ -1,7 +1,22 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+/// <reference types="vitest" />
+/// <reference types="vite/client" />
+import { defineConfig } from 'vite';
+import react from '@vitejs/plugin-react';
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [react()],
+	plugins: [react()],
+	build: {
+		rollupOptions: {
+			external: ['**/*.test.tsx', '*.test.ts'],
+		},
+	},
+	test: {
+		environment: 'jsdom',
+		globals: true,
+		coverage: {
+			reporter: ['text', 'html'],
+			reportOnFailure: true,
+		},
+	}
 })
