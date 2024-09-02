f85182d3ca9d3b674355fef28f464105522fb4db
TatsianaBunevich <tatsiana.bunevich@gmail.com>
2024-09-02 23:50:06 +0200

    add alias paths

--- vite.config.ts
+++ vite.config.ts
@@ -10,6 +10,9 @@ export default defineConfig({
   resolve: {
     alias: {
       '@': path.resolve(__dirname, './src'),
+      '@components': path.resolve(__dirname, './src/components'),
+      '@layout': path.resolve(__dirname, './src/components/layout'),
+      '@ui': path.resolve(__dirname, './src/components/ui'),
     },
   },
   build: {
