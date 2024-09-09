e5b0294b2f33dd528eeced1dfc549c5ab2f6fc6d
TatsianaBunevich <tatsiana.bunevich@gmail.com>
2024-09-09 20:46:36 +0200

    update QuizSkeleton

--- src/components/custom/quiz-skeleton.tsx
+++ src/components/custom/quiz-skeleton.tsx
@@ -26,6 +26,32 @@ const QuizSkeleton = () => {
           <Skeleton key={index} className="h-10 w-full rounded-full" />
         ))}
       </footer>
+      <svg
+        xmlns="http://www.w3.org/2000/svg"
+        version="1.1"
+        className="absolute hidden"
+      >
+        <defs>
+          <filter id="gooey">
+            <feGaussianBlur
+              in="SourceGraphic"
+              stdDeviation="10"
+              result="blur"
+            />
+            <feColorMatrix
+              in="blur"
+              mode="matrix"
+              values="
+        1 0 0 0 0
+        0 1 0 0 0
+        0 0 1 0 0
+        0 0 0 20 -10"
+              result="gooey"
+            />
+            <feComposite in="SourceGraphic" in2="gooey" operator="atop" />
+          </filter>
+        </defs>
+      </svg>
     </>
   )
 }
