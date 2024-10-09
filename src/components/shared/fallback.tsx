862c920746da8b590b4a983a62296c35b15341fa
TatsianaBunevich <tatsiana.bunevich@gmail.com>
2024-10-09 12:18:43 +0200

    update Fallback styles

--- src/components/shared/fallback.tsx
+++ src/components/shared/fallback.tsx
@@ -9,8 +9,8 @@ interface FallbackProps {
 
 const Fallback = ({ error, resetErrorBoundary }: FallbackProps) => {
   return (
-    <main className="container flex h-full flex-col items-center justify-center gap-4">
-      <Alert variant="destructive">
+    <main className="container flex h-full max-w-xl flex-1 flex-col items-center justify-center gap-4">
+      <Alert variant="destructive" className="bg-muted/50">
         <TriangleAlert className="h-4 w-4" />
         <AlertTitle>Error</AlertTitle>
         <AlertDescription>{error.message}</AlertDescription>
