db7942af2deb8975ad43a9b11b3b7cc3e4c54c4e
TatsianaBunevich <tatsiana.bunevich@gmail.com>
2024-10-09 12:17:29 +0200

    add colorful variant to Button

--- src/components/ui/button.tsx
+++ src/components/ui/button.tsx
@@ -18,6 +18,8 @@ const buttonVariants = cva(
           'bg-secondary text-secondary-foreground hover:bg-secondary/80',
         ghost: 'hover:bg-accent hover:text-accent-foreground',
         link: 'text-primary underline-offset-4 hover:underline',
+        colorful:
+          'transition-all duration-500 bg-gradient-to-r from-violet-400 via-fuchsia-300 to-cyan-300 bg-[length:200%_auto] bg-left hover:bg-center',
       },
       size: {
         default: 'h-10 px-4 py-2',
